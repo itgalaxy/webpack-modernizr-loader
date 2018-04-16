af036f4def406b6c50ccd924b3fc43d8c236dccd
Nikita Lobachev <sanniassin@gmail.com>
2018-04-16 13:36:06 +0300

    esModule option

--- index.js
+++ index.js
@@ -3,14 +3,20 @@
 const modernizr = require("modernizr");
 const loaderUtils = require("loader-utils");
 
-function wrapOutput(output) {
+function wrapOutput(output, asEsModule) {
   // Exposing Modernizr as a module.
+  if (asEsModule) {
+    return `var hadGlobal='Modernizr' in window;var oldGlobal=window.Modernizr;${output}export default window.Modernizr;if(hadGlobal){window.Modernizr=oldGlobal;}else{delete window.Modernizr;};`;
+  }
   return `;(function(window){var hadGlobal='Modernizr' in window;var oldGlobal=window.Modernizr;${output}module.exports=window.Modernizr;if(hadGlobal){window.Modernizr=oldGlobal;}else{delete window.Modernizr;}})(window);`;
 }
 
 module.exports = function() {
   const callback = this.async();
   const options = loaderUtils.getOptions(this) || {};
+  const esModule = options.esModule;
+
+  delete options.esModule;
 
   let userConfig = {};
 
@@ -27,5 +33,5 @@ module.exports = function() {
 
   const config = Object.assign({}, userConfig);
 
-  return modernizr.build(config, output => callback(null, wrapOutput(output)));
+  return modernizr.build(config, output => callback(null, wrapOutput(output, esModule)));
 };
